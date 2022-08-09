d130c4b793f38a7eaabdd6a1d0bbf3b9100e746b
Dru-S <drusi97@gmail.com>
2022-08-09 13:42:07 +0200

    Refactor / Opacity / UI
    Done some code refactor, added `opacity` slider to change the column opacity, and improved UI also for narrow screens

--- javascripts/content.js
+++ javascripts/content.js
@@ -5,17 +5,22 @@ if (!window.BGO) {
 
   window.BGO._ = null;
 
+  window.BGO.isDebug = false;
+
   window.BGO.settings = {};
 
   window.BGO.fn = {};
+  window.BGO.fn.debug = (callback) => window.BGO.isDebug && callback();
+
   window.BGO.fn.install = async () => {
 
     // Set some defaults settings
     const bgoDefaultsSettings = {
-      container:   'container-fluid',
-      columns:     24,
+      container:   'container',
+      columns:     12,
+      opacity:     .5,
       background:  'rgb(255, 133, 0)', // RGB value for secondary color in CSS (--bgo-secondary)
-      borderColor: 'rgb(0, 34, 277)', // RGB value for primary color in CSS (--bgo-primary)
+      borderColor: 'rgb(0, 34, 277)',  // RGB value for primary color in CSS (--bgo-primary)
       borderWidth: 0,
     }
 
@@ -28,7 +33,12 @@ if (!window.BGO) {
     }
 
     window.BGO.settings = localStorageSettings.bgo;
-    console.table(window.BGO.settings);
+
+    window.BGO.fn.debug(() => {
+      console.group(`%cwindow.BGO.settings`, `color: coral; font-weight: bold;`)
+      console.table(window.BGO.settings);
+      console.groupEnd();
+    });
 
     const stylesUrl = chrome.runtime.getURL('stylesheets/content.css');
 
@@ -74,61 +84,67 @@ if (!window.BGO) {
         </div>
 
         <div class="bgo_tools">
+
           <div class="bgo_tools__i" data-tool="toggle">
             <button class="bgo_tools__toggle">Toggle</button>
           </div>
 
+          <div class="bgo_tools__i" data-tool="delete">
+            <button>${icons.close}</button>
+          </div>
+
+          <div class="bgo_tools__nl"></div>
+
           <div class="bgo_tools__i" data-tool="columns">
+            <label>Columns</label>
             <input type="number" value="${window.BGO.settings.columns}">
           </div>
 
           <div class="bgo_tools__i" data-tool="container">
+            <label>Container</label>
             <select>
               ${containerOptions}
             </select>
           </div>
 
           <div class="bgo_tools__i" data-tool="background">
-            <label>Background</label>
+            <label>Background<br><span>Color / Opacity</span></label>
             <input type="color" value="${window.BGO.settings.background}">
+            <input type="range" value="${window.BGO.settings.opacity}" min="0" max="1" step="0.01">
           </div>
 
           <div class="bgo_tools__i" data-tool="border">
-            <label>Border</label>
+            <label>Border<br><span>Color / Width</span></label>
             <input type="color" value="${window.BGO.settings.borderColor}">
             <input type="number" value="${window.BGO.settings.borderWidth}">
           </div>
 
-          <div class="bgo_tools__i" data-tool="delete">
-            <button>${icons.close}</button>
-          </div>
         </div>
-
       </div>
     `;
 
-    //
-
+    // Append the element to the body
     document.body.appendChild(window.BGO._);
 
-    //
-
+    // Get some variables
     const wrapper   = window.BGO._.querySelector('.bgo_wrapper');
     const container = window.BGO._.querySelector('.bgo_container');
     const row       = window.BGO._.querySelector('.bgo_row');
     //
     const toolToggle      = window.BGO._.querySelector('.bgo_tools__i[data-tool="toggle"] > button');
+    const toolDelete      = window.BGO._.querySelector('.bgo_tools__i[data-tool="delete"] > button');
     const toolColumns     = window.BGO._.querySelector('.bgo_tools__i[data-tool="columns"] > input');
     const toolContainer   = window.BGO._.querySelector('.bgo_tools__i[data-tool="container"] > select');
     const toolBackground  = window.BGO._.querySelector('.bgo_tools__i[data-tool="background"] > input[type="color"]');
+    const toolOpacity     = window.BGO._.querySelector('.bgo_tools__i[data-tool="background"] > input[type="range"]');
     const toolBorderColor = window.BGO._.querySelector('.bgo_tools__i[data-tool="border"] > input[type="color"]');
     const toolBorderWidth = window.BGO._.querySelector('.bgo_tools__i[data-tool="border"] > input[type="number"]');
-    const toolDelete      = window.BGO._.querySelector('.bgo_tools__i[data-tool="delete"] > button');
 
     // Update the current backgroung and border, by setting some CSS variables
-    window.BGO._.style.setProperty('--bgo-inner-background',   window.BGO.settings.background);
-    window.BGO._.style.setProperty('--bgo-inner-border-color', window.BGO.settings.borderColor);
-    window.BGO._.style.setProperty('--bgo-inner-border-width', `${window.BGO.settings.borderWidth}px`);
+    window.BGO.fn.innerProp('background',   window.BGO.settings.background);
+    window.BGO.fn.innerProp('opacity',      window.BGO.settings.opacity);
+    window.BGO.fn.innerProp('border-color', window.BGO.settings.borderColor);
+    window.BGO.fn.innerProp('border-width', `${window.BGO.settings.borderWidth}px`);
 
     // Basically show the element, without any fancy "animation" (or better, glitch)
     setTimeout(() => {
@@ -136,7 +152,10 @@ if (!window.BGO) {
       wrapper.classList.remove('__loading');
     });
 
+    // Events for "tools"
     toolToggle.addEventListener('click', () => wrapper.classList.toggle('__hidden'));
+
+    toolDelete.addEventListener('click', (e) => window.BGO.fn.uninstall());
 
     toolColumns.addEventListener('input', (e) => {
       window.BGO.settings.columns = +e.target.value;
@@ -155,26 +174,28 @@ if (!window.BGO) {
 
     toolBackground.addEventListener('input', (e) => {
       window.BGO.settings.background = e.target.value;
-      window.BGO.fn.localStorage.set({ bgo: window.BGO.settings }).then(() => {
-        window.BGO._.style.setProperty('--bgo-inner-background',   window.BGO.settings.background);
-      });
+      window.BGO.fn.localStorage.set({ bgo: window.BGO.settings }).then(() =>
+        window.BGO.fn.innerProp('background', window.BGO.settings.background));
+    });
+
+    toolOpacity.addEventListener('input', (e) => {
+      window.BGO.settings.opacity = e.target.value;
+      window.BGO.fn.localStorage.set({ bgo: window.BGO.settings }).then(() =>
+        window.BGO.fn.innerProp('opacity', window.BGO.settings.opacity));
     });
 
     toolBorderColor.addEventListener('input', (e) => {
       window.BGO.settings.borderColor = e.target.value;
-      window.BGO.fn.localStorage.set({ bgo: window.BGO.settings }).then(() => {
-        window.BGO._.style.setProperty('--bgo-inner-border-color', window.BGO.settings.borderColor);
-      });
+      window.BGO.fn.localStorage.set({ bgo: window.BGO.settings }).then(() =>
+        window.BGO.fn.innerProp('border-color', window.BGO.settings.borderColor));
     });
 
     toolBorderWidth.addEventListener('input', (e) => {
       window.BGO.settings.borderWidth = e.target.value;
-      window.BGO.fn.localStorage.set({ bgo: window.BGO.settings }).then(() => {
-        window.BGO._.style.setProperty('--bgo-inner-border-width', `${window.BGO.settings.borderWidth}px`);
-      })
+      window.BGO.fn.localStorage.set({ bgo: window.BGO.settings }).then(() =>
+        window.BGO.fn.innerProp('border-width', `${window.BGO.settings.borderWidth}px`));
     });
 
-    toolDelete.addEventListener('click', (e) => window.BGO.fn.uninstall());
   };
 
   window.BGO.fn.uninstall = () => {
@@ -189,6 +210,9 @@ if (!window.BGO) {
     .fill(true)
     .map(() => `<div class="col"><div class="bgo_inner"></div></div>`)
     .join('');
+
+  window.BGO.fn.innerProp = (key, value) =>
+    window.BGO._?.style.setProperty(`--bgo-inner-${key}`, value);
 
   window.BGO.fn.isFirefox = typeof browser != 'undefined' ? true : false;
 
@@ -216,12 +240,14 @@ if (!window.BGO) {
     },
   };
 
-  console.log(BGO, window.BGO);
-
+  // Install the component
   window.BGO.fn.install();
+
+  window.BGO.fn.debug(() => console.log(window.BGO));
 
 } else {
 
+  // Uninstall the component
   window.BGO.fn.uninstall();
 
 }
